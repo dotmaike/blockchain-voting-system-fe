83e006b3fbadf90f7cd10de6d12f41c92959c0dd
Aldo Valverde <aldo_valverde@epam.com>
2018-03-02 17:36:54 -0600

    Changes folder structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ const App = () => (
       <h1 className="App-title">Welcome to React</h1>
     </header>
     <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
+      To get started, edit <code>src/components/App/App.js</code> and save to reload.
     </p>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import './index.scss';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
