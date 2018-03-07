566a80ea98012474c1dffcee0078c579c5f252d2
Miguel Murillo <miguel_murillo@epam.com>
2018-03-07 15:49:45 -0600

    Added dashboard initial template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.scss';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
