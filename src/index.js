a188947dc2804be4a8adf51612cdff455a99e2e1
Miguel Murillo <miguel_murillo@epam.com>
2018-03-08 13:05:04 -0600

    Added normalize.css & removing index.scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
-import './index.scss';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
