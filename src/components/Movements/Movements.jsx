825cd7c953fcca7a26df3ba9295a0a941b31d197
Daniel Martinez <dmartinez@itexico.net>
2018-03-11 16:58:08 -0600

    Add UI for movements section

--- src/components/Movements/Movements.jsx
+++ src/components/Movements/Movements.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import './styles.scss';
 
-const Movements = () => <div>Movements</div>;
+const Movements = () => (
+  <section className="movement-section">
+    <div className="container has-text-centered">
+      <h1 className="title">Asset Tracking</h1>
+      <div className="movement-form">
+        <div className="field has-addons">
+          <div className="control has-50-vw">
+            <input className="input" type="text" placeholder="Find Assets" />
+          </div>
+          <div className="control">
+            <button className="button is-info">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default Movements;
