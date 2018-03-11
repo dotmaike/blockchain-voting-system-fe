827f3c5c486d5ae5478fa672b6712d02a475df0e
Daniel Martinez <dmartinez@itexico.net>
2018-03-11 15:45:22 -0600

    Change color of icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,22 +5,22 @@ const Footer = () => (
   <footer className="footer">
     <div className="icon-container">
       <p>
-        <a className="icon is-large" href="#email">
+        <a className="icon is-large has-text-grey" href="#email">
           <i className="fa fa-lg fa-envelope" />
         </a>
       </p>
       <p>
-        <a className="icon is-large" href="#twitter">
+        <a className="icon is-large has-text-grey" href="#twitter">
           <i className="fa fa-lg fa-twitter" />
         </a>
       </p>
       <p>
-        <a className="icon is-large" href="#google-plus">
+        <a className="icon is-large has-text-grey" href="#google-plus">
           <i className="fa fa-lg fa-google-plus" />
         </a>
       </p>
       <p>
-        <a className="icon is-large" href="#facebook">
+        <a className="icon is-large has-text-grey" href="#facebook">
           <i className="fa fa-lg fa-facebook-f" />
         </a>
       </p>
