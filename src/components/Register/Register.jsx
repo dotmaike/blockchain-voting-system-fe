5bb796fa65309baaa9e71ba4c086052aefdda3fa
Miguel Murillo <miguel_murillo@epam.com>
2018-03-12 12:07:13 -0600

    Minor adjustment for Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -29,70 +29,62 @@ class Register extends Component {
     return (
       <Fragment>
         <div className="column is-8 messages hero is-fullheight">
-          <div className="card">
-            <div className="card-content">
-              <div className="content">
-                <div className="field">
-                  <label className="label" htmlFor="product-id">
-                    ID
-                  </label>
-                  <div className="control">
-                    <input className="input" id="product-id" type="text" placeholder="ID" />
-                  </div>
-                </div>
-
-                <div className="field">
-                  <label className="label" htmlFor="product-desc">
-                    Product Description
-                  </label>
-                  <div className="control">
-                    <textarea className="textarea" id="product-desc" placeholder="Product Description" />
-                  </div>
-                </div>
-
-                <div className="field">
-                  <section>
-                    <div className="dropzone">
-                      <Dropzone multiple onDrop={this.onDrop}>
-                        <div>
-                          <p>Drop files here...</p>
-                          <i className="fa fa-file fa-3" aria-hidden="true" />
-                        </div>
-                      </Dropzone>
-                    </div>
-                    {this.showFiles()}
-                  </section>
-                </div>
+          <div className="content">
+            <div className="field">
+              <label className="label" htmlFor="product-id">
+                ID
+              </label>
+              <div className="control">
+                <input className="input" id="product-id" type="text" placeholder="ID" />
               </div>
+            </div>
+
+            <div className="field">
+              <label className="label" htmlFor="product-desc">
+                Product Description
+              </label>
+              <div className="control">
+                <textarea className="textarea" id="product-desc" placeholder="Product Description" />
+              </div>
+            </div>
+
+            <div className="field">
+              <section>
+                <div className="dropzone">
+                  <Dropzone multiple onDrop={this.onDrop}>
+                    <div>
+                      <p>Drop files here...</p>
+                      <i className="fa fa-file fa-3" aria-hidden="true" />
+                    </div>
+                  </Dropzone>
+                </div>
+                {this.showFiles()}
+              </section>
             </div>
           </div>
         </div>
 
         <div className="column is-2 messages hero is-fullheight">
-          <div className="card">
-            <div className="card-content">
-              <div className="content">
+          <div className="content">
 
-                <div className="field">
-                  <img src="https://placehold.it/240x240" alt="" />
-                </div>
-
-                <div className="field">
-                  <section>
-                    <div className="dropzone">
-                      <Dropzone multiple onDrop={this.onDrop} accept="image/*">
-                        <div>
-                          <i className="fa fa-file-image-o fa-3" aria-hidden="true" />
-                          <p>Upload Image</p>
-                        </div>
-                      </Dropzone>
-                    </div>
-                    {this.showFiles()}
-                  </section>
-                </div>
-
-              </div>
+            <div className="field">
+              <img src="https://placehold.it/288x288" alt="" />
             </div>
+
+            <div className="field">
+              <section>
+                <div className="dropzone">
+                  <Dropzone multiple onDrop={this.onDrop} accept="image/*">
+                    <div>
+                      <i className="fa fa-file-image-o fa-3" aria-hidden="true" />
+                      <p>Upload Image</p>
+                    </div>
+                  </Dropzone>
+                </div>
+                {this.showFiles()}
+              </section>
+            </div>
+
           </div>
         </div>
       </Fragment>
