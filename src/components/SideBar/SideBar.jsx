9e95267d94a455086c72cb22834653513a0dc7aa
Miguel Murillo <miguel_murillo@epam.com>
2018-03-13 10:07:01 -0600

    Added API request for register

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,31 +3,27 @@ import { NavLink } from 'react-router-dom';
 
 const SideBar = () => (
   <aside className="column is-2 aside hero is-fullheight">
-    <div>
-      <div className="main">
+    <div className="main">
+      <NavLink to="/home" className="item" activeClassName="active">
+        <span className="icon">
+          <i className="fa fa-home" />
+        </span>
+        <span className="name">Home</span>
+      </NavLink>
 
-        <NavLink to="/home" className="item" activeClassName="active">
-          <span className="icon">
-            <i className="fa fa-home" />
-          </span>
-          <span className="name">Home</span>
-        </NavLink>
+      <NavLink to="/register" className="item" activeClassName="active">
+        <span className="icon">
+          <i className="fa fa-edit" />
+        </span>
+        <span className="name">Register</span>
+      </NavLink>
 
-        <NavLink to="/register" className="item" activeClassName="active">
-          <span className="icon">
-            <i className="fa fa-edit" />
-          </span>
-          <span className="name">Register</span>
-        </NavLink>
-
-        <NavLink to="/movements" className="item" activeClassName="active">
-          <span className="icon">
-            <i className="fa fa-history" />
-          </span>
-          <span className="name">Movements</span>
-        </NavLink>
-
-      </div>
+      <NavLink to="/movements" className="item" activeClassName="active">
+        <span className="icon">
+          <i className="fa fa-history" />
+        </span>
+        <span className="name">Movements</span>
+      </NavLink>
     </div>
   </aside>
 );
