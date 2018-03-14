769f566095b96b1e288903d61161f5f3d20d2d99
Miguel Murillo <miguel_murillo@epam.com>
2018-03-14 16:07:04 -0600

    Added request to API

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <nav className="navbar has-shadow">
-    <div className="container">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/home">
-          <p>Welcome !</p>
-        </a>
-      </div>
+import API from './../../Utils/API';
 
-      <div id="navbarExampleTransparentExample" className="navbar-menu">
-        <div className="navbar-end">
-          <a className="navbar-item" href="#share">
-            <span className="icon">
-              <i className="fa fa-share-alt" />
-            </span>
-            <span className="name">Share</span>
-          </a>
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = { userData: {} };
+  }
+  componentWillMount() {
+    API.getUser()
+      .then(data => this.setState({ userData: data }))
+      .catch(error => console.error(error));
+  }
+  render() {
+    return (
+      <nav className="navbar has-shadow">
+        <div className="container">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/home">
+              <p>{Object.keys(this.state.userData).length > 0 && `Welcome ${this.state.userData.username} !`}</p>
+            </a>
+          </div>
 
-          <a className="navbar-item" href="#username">
-            <span className="icon">
-              <i className="fa fa-user-circle" />
-            </span>
-            <span className="name">Username</span>
-          </a>
+          <div id="navbarExampleTransparentExample" className="navbar-menu">
+            <div className="navbar-end">
+              <a className="navbar-item" href="#share">
+                <span className="icon">
+                  <i className="fa fa-share-alt" />
+                </span>
+                <span className="name">Share</span>
+              </a>
 
-          <Link to="/login" className="navbar-item">
-            <span className="icon">
-              <i className="fa fa-sign-out" />
-            </span>
-            <span className="name">Logout</span>
-          </Link>
+              <a className="navbar-item" href="#username">
+                <span className="icon">
+                  <i className="fa fa-user-circle" />
+                </span>
+                <span className="name">Username</span>
+              </a>
+
+              <Link to="/login" className="navbar-item">
+                <span className="icon">
+                  <i className="fa fa-sign-out" />
+                </span>
+                <span className="name">Logout</span>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
+      </nav>
+    );
+  }
+}
 
 export default Header;
