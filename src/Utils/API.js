c4d4d781bd15c1f64be2a66357f1f8fc27aac5e1
Miguel Murillo <miguel_murillo@epam.com>
2018-03-15 18:13:16 -0600

    Random image functionality

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,16 +1,17 @@
 import base64 from 'base-64';
 
+const baseUrl = 'http://10.22.54.104:8080/asset/tracking/';
+const userUrl = 'users/testUser';
+
 const API = {
-  baseUrl: 'http://10.22.54.104:8080/asset/tracking/',
   getUser() {
-    const userUrl = 'users/testUser';
     const username = 'testUser';
     const password = 'password';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Authorization', `Basic ${base64.encode(`${username}:${password}`)}`);
     headers.append('Access-Control-Allow-Origin', '*');
-    return fetch(this.baseUrl + userUrl, {
+    return fetch(baseUrl + userUrl, {
       headers,
       method: 'GET'
     }).then(res => res.json());
