1a37e8686cf5dfd25f06a12cbaa2b27e937b4692
Miguel Murillo <miguel_murillo@epam.com>
2018-03-23 09:50:03 -0600

    Home video

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Home = () => (
   <div className="column is-10 messages hero is-fullheight">
     <iframe
-      title="EPAMers from Mexico"
       width="100%"
       height="100%"
-      src="https://www.youtube.com/embed/Fa4TdimgMdg"
+      src="https://www.youtube.com/embed/C7Oo9QNa0Yc"
       frameBorder="0"
       allow="autoplay; encrypted-media"
       allowFullScreen
+      title="Hyperledger, A Greenhouse Incubator for Blockchain Projects"
     />
   </div>
 );
